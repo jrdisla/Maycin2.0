07bb72c8326e6fac8f1bbf12575919428ca868ca
JulioDisla <20111614@ce.pucmm.edu.do>
2019-08-06 14:03:44 -0400

    db progress

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,34 @@ var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var session = 0;
+  var username = '';
+  if(req.session.user !== undefined)
+  {
+      session = 1;
+      username = req.session.user;
+  }
+  res.render('index', {
+    title: 'Express',
+    session: session,
+    cd : username
+  }
+  );
 });
 
 router.get('/session/:id',function (req,res) {
   console.log("llego");
-  console.log(req.session);
-  if(req.session.user == null)
+  if(req.session.user === undefined)
   {
-    console.log("no hay user");
+      console.log("no hay user"+req.session.user);
       req.session.user = req.params.id;
-      console.log("Setted user");
+      console.log("Setted user: "+req.session.user);
+      res.redirect("/shop/"+req.session.user);
   }
   else
   {
     console.log("hay user");
+    res.redirect("/shop/"+req.session.user);
   }
   // else
   // {
@@ -27,10 +40,19 @@ router.get('/session/:id',function (req,res) {
   //res.redirect("/shop/"+req.session.user);
 
 });
-// router.get('/session', function(req, res, next) {
-//   console.log("id: "+req.session.user);
-//   res.send("El usuario u session es:"+req.session.user)
-// });
+router.get('/session', function(req, res, next) {
+  console.log("llego el user: " +req.session.user);
+  if(req.session.user === undefined)
+  {
+    console.log("no hay user"+req.session.user);
+    //req.session.user = req.params.id;
+    console.log("Setted user");
+  }
+  else
+  {
+    console.log("hay user");
+  }
+});
 
 
 module.exports = router;
